e89c42c5a863f66214e68f3d38d8ea0a67ac8d02
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-29 00:59:09 +0600

    api add and display data using api

--- Module-7(API & DOM)/app.js
+++ Module-7(API & DOM)/app.js
@@ -52,3 +52,29 @@ document.getElementById("handleADD").addEventListener("click",
 // const handleSearch = (event) => {
 //     console.log("Hello Box");
 // }
+
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => {
+        displayData(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+const displayData = (userData) => {
+    const container = document.getElementById("userData-Container");
+
+    userData.forEach((user) => {
+        const div = document.createElement("div");
+        div.classList.add("user")
+
+        div.innerHTML = `
+            <h4>title</h4>
+            <p>Description</p>
+            <button>Details</button>
+        `;
+
+        container.appendChild(div);
+    });
+}

03c1a47fec9cac63bbf9eba8de28a6e38c0a51f5
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-30 15:27:15 +0600

    cart function complete and count the total price

--- Project Using API & DOM/script.js
+++ Project Using API & DOM/script.js
@@ -1,3 +1,5 @@
+
+
 const loadAllProduct = () => {
     fetch('https://fakestoreapi.com/products')
         .then(response => response.json())
@@ -18,11 +20,42 @@ const displayProduct = (products) => {
             <h3>Price: $${product.price}</h3>
             <p>${product.description.slice(0, 100)}</p>
             <button type="">Details</button>
-            <button type="">Add To Cart</button>
+            <button onclick="handleAddToCart('${product.title?.slice(0,12)}', '${product?.price}')" >Add To Cart</button>
         `;
 
         productContainer.appendChild(div);
     });
+}
+
+
+const handleAddToCart = (name, price) =>{
+    // console.log(name, price);
+    const cartCount = document.getElementById("count").innerText;
+    let convertedCount = parseInt(cartCount);
+    convertedCount += 1;
+    document.getElementById("count").innerText = convertedCount;
+    // console.log(convertedCount);
+
+    const container = document.getElementById("cart-main-container");
+
+    const div = document.createElement("div");
+    div.classList.add("cart-info");
+    div.innerHTML = `
+        <p>${name}</p>
+        <h3 class="price">${price}</h3>
+    `;
+
+    container.appendChild(div);
+    updateTotal();
+};
+
+const updateTotal = () =>{
+    const allPrice = document.getElementsByClassName("price");
+    let count = 0;
+    for (const element of allPrice) {
+        count += parseFloat(element.innerText);
+    }
+    document.getElementById("total").innerText = count.toFixed(2);
 }
 
 loadAllProduct();
